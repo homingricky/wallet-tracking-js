cc067810739b6c4d161f6a9c77321c20589045f2
Ricky <rickycheng@genesisblockhk.com>
2022-08-04 12:57:15 +0800

    feat: alert_tg function

---
+++ utils.js
@@ -0,0 +1,32 @@
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+// Telegram Related Functions
+
+export const handle_msg = async function (decodedLog) {
+
+}
+
+
+export const alert_tg = async function (msg) {
+    
+    const data = {
+        chat_id: process.env.TG_CHATROOM_ID,
+        text: String(msg),
+    }
+    console.log(data.text)
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }
+
+    const response = await fetch(process.env.TG_URL, options);
+    const json = await response.json();
+
+}
+
