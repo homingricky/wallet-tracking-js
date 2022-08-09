2aa0eb5b256b7b0fa1a77cf487c0a2a2a7a3e4aa
Ricky <rickycheng@genesisblockhk.com>
2022-08-09 15:52:40 +0800

    feat: addHours & round function

--- tools/utils.js
+++ tools/utils.js
@@ -1,3 +1,13 @@
 export const sleep = (second) => {
     return new Promise ((resolve, reject) => {setTimeout(resolve, second * 10 ** 3)})
 }
+
+export function addHours(numOfHours, date = new Date()) {
+    date.setTime(date.getTime() + numOfHours * 60 * 60 * 1000);
+    return date;
+  }
+
+export function round(num, decimal) {
+    var rounded = Math.round((num + Number.EPSILON) * 10 ** decimal) / (10 ** decimal);
+    return rounded
+}
